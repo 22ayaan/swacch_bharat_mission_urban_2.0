397c5fbbb7721937f2a9ee39d1218f75feffe76d
Ayaan Siddiqui <ars394@pitt.edu>
2023-05-20 10:39:41 +0530

    GeographicInfoPage initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 // import RegisterPage from './components/RegisterPage/RegisterPage.js'
-import LanguagePage from './components/LanguagePage/LanguagePage.js'
+// import LanguagePage from './components/LanguagePage/LanguagePage.js'
+import GeographicInfoPage from './components/GeographicInfoPage/GeographicInfoPage.js'
 
 const App = () => {
   return (
-    <><LanguagePage /></>
+    <><GeographicInfoPage /></>
   )
 }
 
---
+++ src/components/GeographicInfoPage/GeographicInfoPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GeographicInfoPage = () => {
+  return (
+    <>
+      GeographicInfoPage
+    </>
+  )
+}
+
+export default GeographicInfoPage
