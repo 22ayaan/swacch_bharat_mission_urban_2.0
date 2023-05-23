dfaf942ff5aa19aff111bfaf0969053f45c28566
Ayaan Siddiqui <ars394@pitt.edu>
2023-05-23 12:22:22 +0530

    done with get started page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import React from 'react'
 // import LanguagePage from './components/LanguagePage/LanguagePage.js'
 // import GeographicInfoPage from './components/GeographicInfoPage/GeographicInfoPage.js'
 // import SurveyPage1 from './components/SurveyPage/SurveyPage1'
-import SurveyThanksPage from './components/SurveyPage/SurveyThanksPage'
+// import SurveyThanksPage from './components/SurveyPage/SurveyThanksPage'
+import GetStartedPage from './components/GetStartedPage/GetStartedPage'
 
 const App = () => {
   return (
-    <><SurveyThanksPage /></>
+    <><GetStartedPage /></>
   )
 }
 
---
+++ src/components/GetStartedPage/GetStartedPage.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './getStartedPage.css'
+import image from './logo.png'
+const GetStartedPage = () => {
+  return (
+    <>
+      <div className='header'>
+        <p>
+          Swachh Bharat Mission Urban 2.0
+        </p>
+      </div>
+      <div className='container'>
+        <div className='content-wrapper'>
+          <div className='glasses-img'>
+            <img src='https://thewire.in/wp-content/uploads/2017/10/swach.jpg' alt='none' />
+          </div>
+          <div className='heading'>
+            <p className='swachh'>SWACHH</p>
+            <p className='survekshan'>SURVEKSHAN</p>
+            <p className='year'>2023</p>
+          </div>
+          <div className='flag-img'>
+            <img src={image} alt='none' />
+          </div>
+          <div className='desc'>
+            <p className='top-text'>
+              Ministry of Housing and Urban Affairs 
+            </p>
+            <p className='bottom-text'>
+              Government of India
+            </p>
+          </div>
+          <button className='start-btn' type='submit'>Get Started</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default GetStartedPage
